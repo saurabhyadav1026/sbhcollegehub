8018e2a76e8e90ae3e82e987f9e06d235fc772fb
Saurabh Yadav <SAURABHYADAV7041916@GMAIL.COM>
2025-05-30 15:10:51 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,14 @@ import axios from 'axios';
 function App() {
 
   const [users, setUsers] = useState([]);
-
+console.log("sbh in App.js of frontend");
   useEffect(() => {
+    console.log("sbh in useEffect");
     axios.get('https://sbhcollegehub.onrender.com/api/users')
-      .then(res => setUsers(res.data))
-      .catch(err => console.error(err));
+      .then(res => {setUsers(res.data)
+                   console.log("check your data by sbh : "+ res.data);
+                   })
+      .catch(err => console.error("sbh yrr use effect mai hi saari gadbad hai "+err));
 
   }, []);
   return (
@@ -20,6 +23,7 @@ function App() {
 
         <h1>User List</h1>
         {
+          users.length<0:console.log("sbh yrr data to mila nhi kuch"):console.log("data mil gya hai bhai");
             users.length > 0 ? (users.map((u, index) =><p key={index}>{u.name}</p>))
             : (<p> Loading Users....</p>)
             
