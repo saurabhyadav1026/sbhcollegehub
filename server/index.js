95c91b580c60e1ea15581bbc2f2d5babdfec9b80
Saurabh Yadav <SAURABHYADAV7041916@GMAIL.COM>
2025-05-30 13:34:12 +0530

    .0

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({origin:'https://sbhcollegehub.netlify.app/'}));
 app.use(express.json());
 app.use('/api/users',userRoutes);
 
