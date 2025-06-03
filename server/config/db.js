04ba6691288bf4189050edbd69f6e01b822cc2aa
Saurabh Yadav <SAURABHYADAV7041916@GMAIL.COM>
2025-06-03 14:30:41 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -5,8 +5,9 @@ const connectDB = async () => {
   try {
     //const uri = process.env.MONGO_URI;
     await mongoose.connect(process.env.MONGO_URI, {
-     // useNewUrlParser: true,
-     // useUnifiedTopology: true,
+     useNewUrlParser: true,
+      useUnifiedTopology: true,
+      db:'sbhcollegehub'
     });
     console.log('✅ MongoDB connected successfully!');
   } catch (err) {
