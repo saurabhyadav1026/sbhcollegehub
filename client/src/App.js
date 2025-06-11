a19f4a4ec27fa9a702f6ce66055317b04062caa4
Saurabh Yadav <SAURABHYADAV7041916@GMAIL.COM>
2025-06-11 11:42:28 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,15 @@
-/* import './App.css';
 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-
-
-function App() {
-
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-   axios.get('https://sbhcollegehub.onrender.com/api/users')
-     .then(res => setUsers(res.data))
-      .catch(err => console.error(err));
-
-  }, []);
-  return (
-    <div>
-
-        <h1>User List</h1>
-        {
-
-users.length > 0 ? (users.map((u, index) =><p key={index}>{u.name}</p>))
-            : (<p> Loading Users....</p>)
-            
-        }
-      </div>
-  );}
-export default App;
- */
 
 
 import React, { useEffect, useState } from "react";
 import UserForm from "./components/UserForm";
 import UserList from "./components/UserList.js";
 
-const API_URL=process.env.API_URL ; 
+//const API_URL=process.env.API_URL ; 
+
+
+
+const API_URL="https://sbhcollegehub.onrender.com"
 
 function App() {
   const [users, setUsers] = useState([]);
