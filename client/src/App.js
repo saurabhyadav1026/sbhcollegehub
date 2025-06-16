4443fd362d76136bb1b4e6843e502d3ae65769de
Saurabh Yadav <saurabhyadav7041916@gmail.com>
2025-06-16 19:48:47 +0530

    sbh1

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,10 @@
-/* import './App.css';
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-
-
-function App() {
-
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-   axios.get('https://sbhcollegehub.onrender.com/api/users')
-     .then(res => setUsers(res.data))
-      .catch(err => console.error(err));
-
-  }, []);
-  return (
-    <div>
-
-        <h1>User List</h1>
-        {
-
-users.length > 0 ? (users.map((u, index) =><p key={index}>{u.name}</p>))
-            : (<p> Loading Users....</p>)
-            
-        }
-      </div>
-  );}
-export default App;
- */
 
 
 import React, { useEffect, useState } from "react";
 import UserForm from "./components/UserForm";
 import UserList from "./components/UserList.js";
 
-const API_URL=process.env.API_URL ; 
+const API_URL=process.env.REACT_APP_API_URL ; 
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -76,10 +45,10 @@ function App() {
     });
     fetchUsers();
   };
-
+const x="sbh";
   return (
     <div className="container">
-      <h1>User Management</h1>
+      <h1>User Management by sbh</h1>
       <UserForm addUser={addUser} updateUser={updateUser} editingUser={editingUser} />
       <UserList users={users} deleteUser={deleteUser} setEditingUser={setEditingUser} />
     </div>
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 
 import express from "express";
-import connectdb from "./config/db.js";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
+import dotenv from "dotenv";
+import mangoose from "mongoose";
 
 
 
@@ -16,9 +17,21 @@ app.use(express.json());
 // Routes
 app.use("/api/users", userRoutes);
 
+dotenv.config();
 // MongoDB connection
-connectdb();
-
+ try{
+  const uri=process.env.MONGO_URI
+  console.log(uri);
+  mangoose.connect(uri,{
+    useNewUrlParser: true,
+     useUnifiedTopology: true,
+     dbName:'sbhcollegehub'
+   });
+console.log("db connected by sbh");
+}
+catch{
+  console.log("Error sbh connecting to MongoDB");
+}
 app.get('/',(req,res)=>{
   res.setHeader('Content-Type', 'text/html'); 
   res.write("hello bro");
@@ -30,5 +43,5 @@ try{
   app.listen(2030);
 }
 catch(err){
-console.log("sbhydv crashed");
+console.log("sbhydv sbhyd  crashed");
 }
